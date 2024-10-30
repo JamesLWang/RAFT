0490f7e4dd1de2873166e25fd9119fd70a1ad009
James Wang <jameswang1279@gmail.com>
2024-10-29 22:32:50 -0400

    Update docs

--- figures/figure2.py
+++ figures/figure2.py
@@ -13,9 +13,9 @@ for i, ax in enumerate(axs):
         attack = data["result_crits"]
     with open(f"data/xsum_gpt-3.5-turbo.{model}_crits.json", 'r') as f:
         human = json.load(f)
-    ax.hist(attack, bins=10, alpha=0.5, label='GPT-3.5-Turbo + Attack')
-    ax.hist(human, bins=10, alpha=0.5, label='Human')
-    ax.hist(GPT, bins=10, alpha=0.5, label='GPT-3.5-Turbo')
+    ax.hist(attack, bins=10, alpha=0.7, label='GPT-3.5-Turbo + RAFT', color="orange", edgecolor='none', linewidth=0.001)
+    ax.hist(human, bins=10, alpha=0.7, label='Human', color="blue", edgecolor='blue', linewidth=0)
+    ax.hist(GPT, bins=10, alpha=0.7, label='GPT-3.5-Turbo', color="green", edgecolor='green', linewidth=0)
     ax.set_xlabel('Score', fontsize=13)
     ax.set_ylabel('Frequency' if i == 0 else '', fontsize=13)
 
@@ -25,7 +25,7 @@ axs[2].set_title('DetectGPT')
 axs[3].set_title('Fast-DetectGPT')
 
 handles, labels = axs[0].get_legend_handles_labels()
-fig.legend(handles, labels, loc='lower right', ncol=3, fontsize=13)
+fig.legend(handles, labels, loc='best', ncol=3, fontsize=13)
 
 plt.xticks(fontsize=13)
 plt.tight_layout()
